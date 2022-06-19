299237a96ab798846c230e6b152742a06197fcc4
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-06-19 18:38:54 -0300

    test: test_when_read_json_with_normal_data_must_return_data

--- tests/read/read_json_test.py
+++ tests/read/read_json_test.py
@@ -3,17 +3,12 @@ from pytest import mark, raises
 from environment_variables.read import read_json
 
 
-def test_when_read_json_with_valid_file_path_must_return_data():
+def test_when_read_json_with_normal_data_must_return_data():
     data_return = read_json(file_path='tests/environment_test_files/test.json')
 
-    expected_return = [
-        {
-            "key": 1,
-            "value": 1
-        },
-        {
-            "key": 2,
-            "value": 3
-        }
-    ]
+    expected_return = {
+        "key1": 1,
+        "key2": 2
+    }
+
     assert data_return == expected_return
